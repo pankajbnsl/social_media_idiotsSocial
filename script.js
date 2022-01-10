121f3b9c10d91a9dd026f1ff244cb991cc298dc5
pankajbnsl <luckypnkj21@gmail.com>
2022-01-10 22:56:43 +0530

    javscript update

--- script.js
+++ script.js
@@ -15,6 +15,11 @@ const themeModal = document.querySelector('.customize-theme');
 const fontSizes = document.querySelectorAll('.choose-size span');
 var root = document.querySelector(':root');
 
+const colorPalette = document.querySelectorAll('.choose-color span');
+
+const Bg1 = document.querySelector('.bg-1');
+const Bg2 = document.querySelector('.bg-2');
+const Bg3 = document.querySelector('.bg-3');
 
 
 // remove active class from all menu items
@@ -128,4 +133,77 @@ fontSizes.forEach(size => {
 	})
 
 
+})
+
+
+const changeActiveColorClass = () => {
+	colorPalette.forEach(colorPicker => {
+		colorPicker.classList.remove('active');
+	})
+}
+
+
+
+colorPalette.forEach(color => {
+	color.addEventListener('click', () => {
+		let primary;
+		changeActiveColorClass();
+		if(color.classList.contains('color-1')){
+			primaryHue = 252;
+		}else if(color.classList.contains('color-2')){
+			primaryHue = 52;
+		}else if(color.classList.contains('color-3')){
+			primaryHue = 352;
+		}else if(color.classList.contains('color-4')){
+			primaryHue = 152;
+		}else if(color.classList.contains('color-5')){
+			primaryHue = 202;
+		}
+		color.classList.add('active');
+		root.style.setProperty('--primary-color-hue', primaryHue);
+	})
+})
+
+
+let lightColorLightness;
+let whiteColorLightness;
+let darkColorLightness;
+
+const changeBG = () => {
+	root.style.setProperty('--light-color-lightness', lightColorLightness);
+	root.style.setProperty('--white-color-lightness', whiteColorLightness);
+	root.style.setProperty('--dark-color-lightness', darkColorLightness);
+}
+
+Bg1.addEventListener('click', () => {
+
+	Bg1.classList.add('active');
+	// remove active class from the others
+	Bg2.classList.remove('active');
+	Bg3.classList.remove('active');
+	window.location.reload();
+})
+
+Bg2.addEventListener('click', () => {
+	darkColorLightness = '95%';
+	whiteColorLightness = '20%';
+	lightColorLightness = '15%';
+
+	Bg2.classList.add('active');
+	// remove active class from the others
+	Bg1.classList.remove('active');
+	Bg3.classList.remove('active');
+	changeBG();
+})
+
+Bg3.addEventListener('click', () => {
+	darkColorLightness = '95%';
+	whiteColorLightness = '10%';
+	lightColorLightness = '0%';
+
+	Bg2.classList.add('active');
+	// remove active class from the others
+	Bg2.classList.remove('active');
+	Bg1.classList.remove('active');
+	changeBG();
 })
